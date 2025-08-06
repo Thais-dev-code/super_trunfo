13fe13381f12951d4f84ce58b8d44571cd2eb05a
Thais-dev-code <tmara.dev@gmail.com>
2025-08-06 09:39:43 -0300

    Create super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  printf ("Bem-vindo ao Super Trunfo das Cidades! \n");
+  printf("Execute um dos arquivos para visualizar os dados. \n");
+  return 0;
+
+
+
+
+
+
+  
+}
